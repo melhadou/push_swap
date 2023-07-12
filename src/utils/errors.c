a091a20c43068bcda79b9048bb2c9a2776faa103
Mohamed Elhadouchi <melhadouchi387@gmail.com>
2023-07-12 16:27:06 +0100

    Update: parse args, and init stack

--- src/utils/errors.c
+++ src/utils/errors.c
@@ -1,39 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   errors.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: melhadou <melhadou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/08 11:32:08 by melhadou          #+#    #+#             */
-/*   Updated: 2023/07/12 16:24:49 by melhadou         ###   ########.fr       */
+/*   Created: 2023/07/12 15:32:40 by melhadou          #+#    #+#             */
+/*   Updated: 2023/07/12 15:33:13 by melhadou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int main(int argc, char *argv[])
+void 	ft_error(void)
 {
-	t_stack *a;
-	t_stack *b;
-
-	a = NULL; // Initialize a to NULL
-	b = NULL; // Initialize b to NULL
-	if (argc == 1)
-		return (0);
-	else
-	{
-		init_stack(&a, argv);
-		// check number if there are sorted
-		// sort_three(&a);
-		// if (check_is_ranked(a))
-		// 	send_all(&a, &b);
-		// else
-		// {
-		// 	exit(1);
-		// }
-		ft_print_stacks(a, b);
-	}
-	system("mtrace push_swap");
-	return (0);
+	write(2, "Error\n", 6);
+	exit(1);
 }
